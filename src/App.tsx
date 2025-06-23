3cbaeab8f12fa8a31d290766ed7348c4d7f48770
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-06-23 22:43:55 +0900

    [DESIGN] Common Button 공통 컴포넌트 제작  (#6)
    * design: common button templete
    
    * chore: src 전역 경로 추가
    
    * design&feat: 전역에서 사용하는 구글 아이콘 박스 컴포넌트 추가
    
    * design: CommonButton WithIcon 속성 추가
    - 사용자가 원하는 아이콘이 있다면 아이콘props에 IconBox태그를 넣어 전달하면 됨
    - App.ts에 테스트용 코드 추가
    
    * design: CommonButton Disable 속성 추가
    
    * remove: common 폴더 하위로 이동
    
    * design: Common 버튼 디자인 일부 수정
    - max width 추가
    - 단어 단위 줄바꿈 추가
    
    * design: Textbutton 추가
    - 일정 생성 완료 화면 등 에서 좌측에 생기는 눈에 안띄는 글자 버튼
    
    * comment: add css default setting description
    
    * design: small button components 추가
    - App.tsx 에 테스트 코드 추가
    
    * refactor: story book common button 속성 이름 변경
    
    * refactor: IconBox 모듈 선언을 실제 컴포넌트 인터페이스와 경로에 맞게 수정
    
    * design: 접근성을 위해 키보드 포커싱 시 outline 추가
    
    * refactor: TextButton 기본 속성 정의 및 aria-label 등 접근성 개선 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import { useState } from "react";
-import { CommonTag } from "./components/common/tags/commonTag";
-import { SelectTag } from "./components/common/tags/SelectTag";
+import { CommonTag } from "@/components/common/tags/commonTag";
+import { SelectTag } from "@/components/common/tags/SelectTag";
+import CommonButton from "@/components/common/buttons/CommonButton";
+import IconBox from "@/components/common/IconBox";
+import TextButton from "@/components/common/buttons/TextButton";
+import SmallButton from "@/components/common/buttons/SmallButton";
 
 function App() {
   const [active, setActive] = useState<boolean>(true);
@@ -18,6 +22,15 @@ function App() {
       />
       <SelectTag label="테스트2" onClick={() => setActive(!active)} />
       <p className="text-header leading-header tracking-header">제목</p>
+
+      <TextButton label="일정 다시 만들기" />
+      <CommonButton
+        label="테스트 버튼"
+        onClick={() => alert("버튼 클릭됨")}
+        icon={<IconBox name="home" width={24} height={24} />}
+        isDisabled={false}
+      />
+      <SmallButton label="테스트버튼" />
     </div>
   );
 }
